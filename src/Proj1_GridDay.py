58ca18a8185f176a7a4f9b0dc5f92b116bb3c65d
jakeangus <60803490+jakeangus@users.noreply.github.com>
2021-09-13 22:26:32 -0500

    Implemented vertical part of the placeShip method
    Invoked nested ifs from 268 to check for good input
    
    Co-Authored-By: FreemanSpray <89803643+FreemanSpray@users.noreply.github.com>

--- src/Proj1_GridDay.py
+++ src/Proj1_GridDay.py
@@ -6,6 +6,7 @@ class Grid:
     #creates 2D array which acts as the board
       rows, cols = (11, 11)
       self.grid = [["X" for i in range(cols)] for j in range(rows)]
+      global alphabet
       alphabet = ["A","B","C","D","E","F","G","H","I","J"]
       self.grid[0][0] = "  "
       for i in range(1,11):
@@ -24,12 +25,35 @@ class Grid:
 
         return(self.grid[xPos][yPos])
 
-    def placeShip(self, type, orientation, xPos, yPos):
-        pass
-        #Called when setting up the board. Takes the type of ship, along with the orientation and coordinates for the leftmost or topmost coordinate of the ship.
+       
+    #Called when setting up the board. Takes the type of ship, along with the orientation and coordinates for the leftmost or topmost coordinate of the ship.
+    def placeShip(self, size, orientation, row, col):
+        success = False
+        fail = False
+        while not success:
+            if orientation:
+                if row in alphabet and col in range(1,10):
+                    if alphabet.index(row) + size < 10: 
+                        for i in range(alphabet.index(row), alphabet.index(row) + size):
+                            if self.grid[i+1][col] != "X":
+                            #TODO: do something other than set success to true here
+                                fail = True
+                                
+                        if fail:
+                           break
+
+                        for i in range(alphabet.index(row), alphabet.index(row) + size):
+                            if self.grid[i+1][col] == "X":
+                                self.grid[i+1][col] = str(size)
+                                success = True
+            elif not orientation:
+                #TODO: Add horizontal orientation code here
+                pass
+
+
         
-    def fireShot(self, xPos, yPos):
     #asserts whether a shot at a given coordinate is a hit or miss
+    def fireShot(self, xPos, yPos):
         if(self.grid[xPos][yPos] == "2" or self.grid[xPos][yPos] == "3" or self.grid[xPos][yPos] == "4" or self.grid[xPos][yPos] == "5"):
             print("Hit!")
         else:
@@ -41,3 +65,6 @@ grid1.printBoard()
 print(grid1.getStatus(1,5))
 
 grid1.fireShot(0,4)
+grid1.placeShip(5, 1, "D", 5)
+grid1.placeShip(4, 1, "A", 5 )
+grid1.printBoard()
